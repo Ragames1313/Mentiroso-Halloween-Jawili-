941f664fd2164b51ed16a0a3c5e19ca1e3887d68
Cristian Gil Gz <chipsentinel@gmail.com>
2025-11-05 13:51:47 +0100

    Cree una static Random para para no tener que estar repitiendo constantemente, tambien cree varios grupos de statick por que por lo que aprendi con santiago cuanto mas static o mas definiciones de autocodigo mas facil de leer a la larga y de repartir el trabajo

--- Mentiroso Halloween/src/Main.java
+++ Mentiroso Halloween/src/Main.java
@@ -3,69 +3,96 @@ import java.util.Scanner;
 
 public class Main {
 
-//Para generar numeros aleatorios
-    public static int generarnumaleatoriorangos(int p_iMinValue, int p_iMaxValue) {
-        int iRet = p_iMaxValue;
-        Random rnd = new Random();
-        iRet = rnd.nextInt((p_iMaxValue + 1) - p_iMinValue) + p_iMinValue;
-        return iRet;
+    //Creamos un objeto random global para no tenier que designar random en cada momento
+    static Random rnd = new Random();
+
+    //Para generar numeros aleatorios entre min y max
+    public static int generarNumAleatorioRangos(int minValue, int maxValue) {
+
+        //Dado que hemos designado random como global anteriormente podemos simplificar en una linea
+        return rnd.nextInt((maxValue + 1) - minValue) + minValue;
+    }
+
+    //Para generar nummeros aleatorios en la manos (de cartas)
+    static void rellenarManos (int[][] manos) {
+        for (int i = 0; i < manos.length; i++) {    //  recorre la columna
+            for (int j = 0; j < manos[i].length; j++) {   // ponemos manos[i].length en vez de 5 (recorre la fila)
+                manos[i][j] = generarNumAleatorioRangos(0, 6);
+            }
+        }
+    }
+
+    //Para mostrar en pantalla las manos a los jugadores
+    public static void  mostrarManos(int[][] mano){
+        for (int i = 0; i < mano.length; i++) {
+            System.out.print("Mano del jugador " + (i + 1) + ": ");
+            for (int j = 0; j < mano[i].length; j++) {
+                   System.out.print(mano[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    //For para enseñar las cartas en la mesa
+    public static void mostrarMesa(int[][] mesa) {
+        System.out.print("Mesa: ");
+        for (int j = 0; j < mesa[0].length; j++) {  //Recorremos las columnas de la fila 0.
+            System.out.print(mesa[0][j] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int[][] mano = new int[2][5];
-        int[][] mesa = new int[1][5];
+        //Definimos los dogmas del codigo "final" bloquea la cantidad asignada de la constante
+        final int numJugadores = 2;
+        final int cartasPorJugador = 5;
+
+
+        //Matriz mano [2][5]
+//        int[][] mano = new int[2][5];
+        int[][] mano = new int [numJugadores][cartasPorJugador];
+
+        //Matriz mesa [1][5]
+//        int[][] mesa = new int[1][5];
+        int[][] mesa = new int[1][cartasPorJugador];
+
         int turno = 1;
-        int otra = 1;
 
-//Reglas del juego
-        System.out.println("Bienvenido de MENTIROSO HALLOWEEN");
-        System.out.println("�� Reglas del juego\n" +
-                "�� Preparación \n" +
-                "∙ Cada jugador tiene una mano representada por una fila de la matriz  mano[2][5]. \n" +
-                "∙ Las cartas son números del 0 al 6, asignados aleatoriamente al inicio: o 1–5: calabazas normales \n" +
-                "o 6: calabaza maldita \n" +
-                "o 0: fantasma (comodín) \n" +
-                "∙ La mesa se representa con una matriz mesa[1][5], donde se colocan las  cartas declaradas. \n" +
-                "��♂\uFE0F Turnos \n" +
-                "1. El jugador activo declara que pone X cartas del mismo valor sobre la  mesa. \n" +
-                "2. El otro jugador puede:  \n" +
-                "o Creerle y continuar el juego. \n" +
-                "o Dudar y comprobar si decía la verdad. \n" +
-                "�� Resolución \n" +
-                "∙ Si el jugador mentía, pierde una carta. \n" +
-                "∙ Si decía la verdad, el que dudó pierde una carta. \n" +
-                "∙ Si se juega una calabaza maldita (6) y el otro duda, pierde 2 cartas. \n" +
-                "∙ Si se juega un fantasma (0) y se descubre, el jugador pierde toda su  mano. \n" +
-                "∙ El juego termina cuando un jugador se queda sin cartas. ");
-
-        //Para generar la mano
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 5; j++) {
-                mano[i][j] = generarnumaleatoriorangos(0, 6);
-            }
+        // Inicializamos la mesa con -1 porq 0 es una carta fantasma
+        for (int j = 0; j < cartasPorJugador; j++){
+            mesa[0][j] = -1;
         }
 
-        //Para enseñarles las manos a los jugadores
-        for (int i = 0; i < 2; i++) {
-            System.out.print("mano del jugador " + (i+1) + ": ");
-            for (int j = 0; j < 5; j++) {
-                System.out.print(mano[i][j] + " ");
-            }
-            System.out.println();
-        }
-//Un do para los turnos
+//        int otra = 1;
+
+        //Reglas del juego
+        System.out.println("Bienvenido a MENTIROSO HALLOWEEN");
+        System.out.println("Reglas del juego (resumen):");
+        System.out.println(" - Cada jugador tiene 5 cartas (0 a 6).");
+        System.out.println(" - Por turnos, los jugadores ponen cartas en la mesa.");
+        System.out.println(" - Más adelante se gestionará si mienten, dudan, etc.\n"); //Salto de linea "\n"
+
+        //Repartimos cartas
+        rellenarManos(mano);
+
+        //Mostrar manos
+        mostrarManos(mano);
+
+        //Un do para los turnos
         //do (Ya irá)
         //{
-        //if para marcar el turno del jugador uno o el jugador dos
-           if (turno % 2 != 0)
-           {
-               //Preguntarle al usuario si quiere sacar las cartas y asignarlas a la mesa en cuyo caso
+        //Marcar el turno del jugador uno o el jugador dos
+           if (turno % 2 != 0){
                System.out.print("Turno del jugador 1! \n");
-               for (int j = 0; j < 5; j++) {
-                   System.out.println("Deseas tirar tu carta " + j + " ? (1 = SI 2 = NO)");
+
+               //Pregunta al usuario si quiere sacar las cartas y asignarlas a la mesa en cuyo caso
+               //Deberia ser sustituido por un boleano para mejorar el codigo
+               for (int j = 0; j < cartasPorJugador; j++) {
+                   System.out.println("Quieres tirar tu carta " + (j+1) + "(valor " + mano[0][j] + ")? (1 = SI 2 = NO)");
                    int respuesta = sc.nextInt();
+
                    if (respuesta == 1)
                    {
                        mesa[j] = mano[j];
@@ -75,17 +102,17 @@ public class Main {
            }
         //} while (tambien irá luego)
 
-        //for para enseñar las cartas en la mesa
-        for (int i = 0; i < 1; i++) {
-            System.out.print("mesa: ");
-            for (int j = 0; j < 5; j++) {
-                System.out.print(mesa[j] + " ");
-            }
-        }
-            System.out.println();
-        }
+        //Mostramos como ha quedado la mesa
+        System.out.println();
+        mostrarMesa(mesa);
 
+        //Mostramos mano
+        mostrarManos(mano);
+
+        //Cerramos Scanner
+        sc.close();
     }
+}
 
     /*static boolean respuestaSi(Scanner sc) {
         while (true) {
